f141a5b0f4f415ecd0587e54b3ef9db0bc8b9cc1
torikraju <torikraju@hotmail.com>
2017-11-11 11:52:33 +0600

    Adding driver setup and default code

---
+++ src/main/java/WebDriverSetup/WebDriverSetup.java
@@ -0,0 +1,26 @@
+package WebDriverSetup;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverSetup {
+
+    private WebDriver webDriver;
+
+    public WebDriver intialization(String browserName) {
+        if (browserName.equalsIgnoreCase("Firefox")) {
+            System.setProperty("webdriver.gecko.driver", "resources/drivers/geckodriver.exe");
+            webDriver = new FirefoxDriver();
+        } else if (browserName.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "resources/drivers/geckodriver.exe");
+            webDriver = new ChromeDriver();
+        } else {
+            System.out.println("browserName = [" + browserName + "] is not valid");
+           // return null;
+        }
+
+        return webDriver;
+    }
+
+}
---
+++ src/test/java/com/torikraju/Default/Default.java
@@ -0,0 +1,27 @@
+package com.torikraju.Default;
+
+import WebDriverSetup.WebDriverSetup;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+public class Default {
+
+    private WebDriver driver;
+
+    @Before
+    public void init() {
+
+        driver = new WebDriverSetup().intialization("firefox");
+    }
+
+    @Test
+    public void tests() {
+    }
+
+    @After
+    public void close() throws InterruptedException {
+        driver.close();
+    }
+}
